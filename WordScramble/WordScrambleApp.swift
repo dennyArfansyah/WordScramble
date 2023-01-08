75ccda53a5b881bea18fe6b5a90ef3befdf70940
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-08 22:08:59 +0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Denny Arfansyah on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
